2b92525c199a3a95f246bfe4f515268620ad5684
pitbrest <sasha.petrov@tut.by>
2022-05-09 23:24:05 +0300

    feat: add language save and toggle

---
+++ js/language-toggle.js
@@ -0,0 +1,37 @@
+import { keyboardRender } from './keybord.js'
+
+// Сохраняем в localStorage выбранный пользователем язык ввода, и задаем сочетание клавиш для его изменения
+
+let counter = [];
+
+function languageToggle() {	
+
+	if (!localStorage.getItem('lang')) {
+		localStorage.setItem('lang', 'en');
+	}
+
+	document.addEventListener('keydown', (e) => {
+		if (e.repeat) {
+			return;
+		};
+		counter.push(e.code);
+	})
+
+	document.addEventListener('keyup', (e) => {
+		if (counter.length === 0) {
+			return;
+		}
+		if (counter.length == 2 && counter.includes('ControlLeft') && counter.includes('ShiftLeft')) {
+			if (localStorage.lang === 'en') {
+				localStorage.lang = 'ru';
+				keyboardRender();
+			} else {
+				localStorage.lang = 'en';
+				keyboardRender();
+			}
+		}
+		counter.length = 0;
+	})
+}
+
+languageToggle();
